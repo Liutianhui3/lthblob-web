9584cb96bcd01e4add4e0818f40293eba1da341e
刘添慧 <tianhui.liu@shanghairanking.com>
2023-11-21 15:31:45 +0800

    section3滚动渐显效果实现

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -18,7 +18,42 @@
               </div>
             </div>
           </section>
-          <section class="section-tvc"></section>
+          <section class="section-tvc">
+            <div class="section-video-wrapper aspect-ratio">
+              <div class="section-inline-video">
+                <video autoplay loop playsinline>
+                  <source type="video/mp4" src="@/assets/videos/section2.mp4" />
+                </video>
+              </div>
+            </div>
+          </section>
+          <section class="section-magic">
+            <div class="section-wrapper">
+              <div class="section-headline fade-copy fade-trigger">4大技术加持 共筑新体验</div>
+              <div class="section-content fade-copy fade-trigger">
+                <div class="section-item">
+                  <img class="section-icon" src="@/assets/images/icon-magic-ring.svg" alt="" />
+                  <h3 class="section-headline-reduced">MagicRing 信任环</h3>
+                  <p class="section-intro">跨系统可信互联</p>
+                </div>
+                <div class="section-item">
+                  <img class="section-icon" src="@/assets/images/icon-magic-ring.svg" alt="" />
+                  <h3 class="section-headline-reduced">Magic Live 智慧引擎</h3>
+                  <p class="section-intro">平台级AI能力</p>
+                </div>
+                <div class="section-item">
+                  <img class="section-icon" src="@/assets/images/icon-magic-ring.svg" alt="" />
+                  <h3 class="section-headline-reduced">Turbo X 系统引擎</h3>
+                  <p class="section-intro">内核级底层性能增强</p>
+                </div>
+                <div class="section-item">
+                  <img class="section-icon" src="@/assets/images/icon-magic-ring.svg" alt="" />
+                  <h3 class="section-headline-reduced">MagicGuard 荣耀安全</h3>
+                  <p class="section-intro">三重安全防护体系</p>
+                </div>
+              </div>
+            </div>
+          </section>
         </div>
       </div>
     </div>
@@ -43,7 +78,7 @@
         return gsap
           .timeline({
             scrollTrigger: {
-              start: 'top80%', // 元素顶部进入视口80%时触发动画
+              start: 'top 0%', // 元素顶部进入视口80%时触发动画
               toggleActions: 'play none none reverse'
             }
           })
@@ -62,8 +97,24 @@
     }
   })
 
+  const triggerFn = () => {
+    const triggerList = document.querySelectorAll('.fade-trigger')
+    triggerList.forEach(item => {
+      const hook = item.getAttribute('data-hook') || '70%'
+      gsap.timeline({
+        scrollTrigger: {
+          trigger: item,
+          start: 'top ' + hook,
+          toggleClass: 'active'
+          // markers: true,
+        }
+      })
+    })
+  }
+
   onMounted(() => {
     gsap.timeline().rainbow('h2')
+    triggerFn()
   })
 </script>
 <style lang="less" scoped>
@@ -162,15 +213,100 @@
             }
           }
           .section-tvc {
-            height: 100vh;
             width: 100%;
+            position: relative;
+            z-index: 1;
+            background: #fff;
+            .section-video-wrapper {
+              width: 100%;
+              position: relative;
+              padding-bottom: 56.25%;
+              .section-inline-video {
+                overflow: hidden;
+                line-height: 0;
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                object-fit: contain;
+                video {
+                  width: 100%;
+                  max-width: 100%;
+                  display: inline-block;
+                  object-fit: contain;
+                  overflow-clip-margin: content-box;
+                  overflow: clip;
+                }
+              }
+            }
           }
           .section-dark {
             background: #000;
             color: #fff;
           }
+          .section-magic {
+            position: relative;
+            z-index: 1;
+            background: #fff;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            width: 100%;
+            height: 100vh;
+            text-align: center;
+            .section-wrapper {
+              .section-headline {
+                width: 100%;
+                font-size: 2.5vw;
+                font-weight: 500;
+                color: rgba(0, 0, 0, 0.8) !important;
+                font-weight: 400;
+              }
+              .section-content {
+                display: flex;
+                flex-wrap: wrap;
+                justify-content: center;
+                width: 100%;
+                margin-top: 7.291667vw;
+                .section-item {
+                  position: relative;
+                  padding: 0 3.333333vw;
+                  .section-icon {
+                    width: 5.20833vw;
+                    height: auto;
+                  }
+                  .section-headline-reduced {
+                    font-size: 1.666667vw;
+                    margin: 0;
+                    line-height: 1.2;
+                    color: rgba(0, 0, 0, 0.8);
+                    font-weight: 500;
+                    font-family: HYQiHei70S !important;
+                  }
+                  .section-intro {
+                    font-size: 0.9375vw;
+                    margin-top: 1.041667vw;
+                    color: rgba(0, 0, 0, 0.5);
+                  }
+                }
+              }
+            }
+          }
         }
       }
+    }
+  }
+
+  .fade-copy {
+    transition:
+      opacity 0.5s,
+      transform 0.5s;
+    transform: translateY(50px);
+    opacity: 0;
+    &.active {
+      transform: translateY(0px);
+      opacity: 1;
     }
   }
 </style>
